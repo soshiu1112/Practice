4cc75097ba775cb19c9c2b832e621386b2d02856
soshiu1112 <soshiu1112@gmail.com>
2023-10-10 04:16:08 +0900

    all blog view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog</title></title>
+        <!--<link rel="stylesheet" href="">-->
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            @foreach ($posts as $post)
+            <div class="post">
+                <h2 class="title">{{$post->title}}</h2>
+                <p class="body">{{$post->body}}</p>
+            @endforeach
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use App\Http\Controllers\PostController;
 */
 
 /*Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });*/
+Route::get('/',[PostController::class,'index']);
 
 Route::get('/posts',[PostController::class,'index']);
