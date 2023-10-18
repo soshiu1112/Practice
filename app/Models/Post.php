3abb432644ce2f285741271e231a1aeacd7717a0
soshiu1112 <soshiu1112@gmail.com>
2023-10-19 05:30:01 +0900

    blog delete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,18 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     protected $fillable = [
         'title',
         'body',
+        'category_id'
         ];
     use HasFactory;
+    use SoftDeletes;
+    
     public function getByLimit(int $limit_count = 10){
         return $this->orderBy('updated_at','DESC')->limit($limit_count)->get();
     }
     public function getPaginateByLimit(int $limit_count = 3){
-        return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,12 +9,17 @@
         <h1 class="title">
             {{$post->title}}
         </h1>
+        <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
         <div class="content">
             <div class="content_text">
                 <h3>本文</h3>
                 <p>{{$post->body}}</p>
             </div>
         </div>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
+        </div>
+
         <div class="footer">
             <a href=".">戻る</a>
         </div>
